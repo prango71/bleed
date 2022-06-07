59434ff1f29dc1183e01e59ec46b95f49bdab9bc
prango71 <101040779+prango71@users.noreply.github.com>
2022-06-07 15:23:42 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@ git config --global user.email "pranshug.us@gmail.com"
 git config --global user.name "pranshoe"
 git config --global color.ui false
 
-repo init -u https://github.com/P-404/android_manifest -b shinka --depth=1
+repo init -u https://github.com/Spark-Rom/manifest -b spark --depth=1
 git clone https://github.com/datnerdguy/local_manifest --depth=1 -b 12-4.19 .repo/local_manifests
 repo sync -c --force-sync -j16 --no-clone-bundle --no-tags
