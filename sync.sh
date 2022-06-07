fdd268f8df8fcb43ff0a9455fb66f2de74267555
prango71 <101040779+prango71@users.noreply.github.com>
2022-06-07 15:21:56 +0530

    [skip ci]

--- sync.sh
+++ sync.sh
@@ -1,12 +1,7 @@
-git config --global user.email "flynryder427@gmail.com"
-git config --global user.name "xenxynon"
+git config --global user.email "pranshug.us@gmail.com"
+git config --global user.name "pranshoe"
 git config --global color.ui false
 
-
-# sync
-repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
-git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
-repo sync -c --force-sync -j8 --no-clone-bundle --no-tags
-rm -rf packages/resources/devicesettings
-git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
-# build????
+repo init -u https://github.com/P-404/android_manifest -b shinka --depth=1
+git clone https://github.com/datnerdguy/local_manifest --depth=1 -b 12-4.19 .repo/local_manifests
+repo sync -c --force-sync -j16 --no-clone-bundle --no-tags
